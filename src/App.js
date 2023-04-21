6cb7088595ba2bff3081ada55f6921a45ac01b27
mhmdrmd97 <68133073+mhmdrmd97@users.noreply.github.com>
2023-04-21 21:35:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './App.css';
 import MySortableList from './MySortableList';
+import { Button } from '@mui/material';
+import { useSetAtom } from 'jotai';
+import { fieldsAtom } from './atoms';
 
 function App() {
+  const setFields = useSetAtom(fieldsAtom)
   return (
-    <div><MySortableList /></div>
+    <div>
+      <MySortableList />
+      <Button onClick={()=>setFields((fields)=>[...fields,{id:`${Math.random()}`,type:"New"}])} >
+        Add
+      </Button>
+    </div>
   )
 }
 
