f512880a3785a588f8652036a6cc5c125d86a200
mhmdrmd97 <68133073+mhmdrmd97@users.noreply.github.com>
2023-04-21 20:20:02 +0300

    Now it Takes Objects as parameters

--- src/MySortableList.js
+++ src/MySortableList.js
@@ -16,7 +16,7 @@ import { Card } from 'react-bootstrap';
 
 function MySortableList() {
   //replace by jotai
-    const [languages, setLanguages ] = useState(["JavaScript", "Python", "TypeScript"]);
+    const [fields, setFields ] = useState([{id:"1",type:"JavaScript"}, {id:"2",type:"Python"}, {id:"3",type:"TypeScript"}]);
 
   return (
     <DndContext
@@ -24,20 +24,20 @@ function MySortableList() {
       onDragEnd={handleDragEnd}
     >
       <Container className="p-3" style={{"width": "50%"}} align="center">
-        <h3>The best programming languages!</h3>
+        <h3>The best programming fields!</h3>
         <SortableContext
-          items={languages}
+          items={fields}
           strategy={verticalListSortingStrategy}
        
        > 
        
           {/* We need components that use the useSortable hook */}
-          {languages.map((language,index) =><div>
+          {fields.map((field,index) =><div key={field.id*Math.random()}>
               
-            <SortableItem  topHandle={0} leftHandle={"50%"}  key={language} id={language}>
+            <SortableItem  topHandle={0} leftHandle={"50%"}   {...field} >
             <Card body className="m-3">
                
-               {language}
+               {field.type}
                </Card>
             </SortableItem>
           </div> 
@@ -49,24 +49,14 @@ function MySortableList() {
   );
 
   function handleDragEnd(event) {
-
-    //handle logic and set to jotai
-
-    console.log("Drag end called");
     const {active, over} = event;
-    console.log("ACTIVE: " + active.id);
-    console.log("OVER :" + over.id);
-
-    if(active.id !== over.id) {
-      setLanguages((items) => {
-        const activeIndex = items.indexOf(active.id);
-        const overIndex = items.indexOf(over.id);
-        console.log(arrayMove(items, activeIndex, overIndex));
+    if (active.id !== over.id) {
+      setFields((items) => {
+        const activeIndex = items.findIndex(item => item.id === active.id);
+        const overIndex = items.findIndex(item => item.id === over.id);
+        console.log(arrayMove(items, activeIndex, overIndex))
         return arrayMove(items, activeIndex, overIndex);
-        // items: [2, 3, 1]   0  -> 2
-        // [1, 2, 3] oldIndex: 0 newIndex: 2  -> [2, 3, 1] 
       });
-      
     }
   }
 }
